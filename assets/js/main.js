778322ae84604da8a5324d310c0dbfae22ac1cce
FrancioXavier <francioxaviersilva@gmail.com>
2023-02-02 13:31:10 -0300

    test 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,26 @@
-
+async function fetchProfileData(){
+    const url = 'https://raw.githubusercontent.com/FrancioXavier/Presente/main/data/photos.json';
+    const response = await fetch(url)
+    const profileData = await response.json();
+    return profileData;
+}
 
 function updatePhotos(photoData){
     const photo = document.getElementById('photos');
-    photo.innerHTML = photoData.photo.map(photo => {
-        `
-        ${photo}
-        `
-    });
+    photo.innerHTML = photoData.content.map(photo =>  `
+    <li class="item">
+        <div>
+            <div class="polaroid"><img src="${photo.image}">
+                <div class="caption">${photo.description}</div>
+            </div>
+        </div>
+    </li>
+    `).join('');
     
 }
 
 (async () => { 
 
     const photoData = await fetchProfileData();
-    updatePhotos(photoData)
+    updatePhotos(photoData);
 })()
-
-{/* <li class="item">
-          <div>
-            <div class="polaroid"><img src="${photo.image}">
-              <div class="caption">${photo.description}</div>
-            </div>
-          </div>
-        </li> */}
