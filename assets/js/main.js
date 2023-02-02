fdc8abab02800daaa7204f41b35deb37c9037e0d
FrancioXavier <francioxaviersilva@gmail.com>
2023-02-02 16:30:36 -0300

    almost final result

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,22 +5,20 @@ async function fetchProfileData(){
     return profileData;
 }
 
-function updatePhotos(photoData){
-    const photo = document.getElementById('photos');
-    photo.innerHTML = photoData.content.map(photo =>  `
-    <li class="item">
-        <div>
-            <div class="polaroid"><img src="${photo.image}">
-                <div class="caption">${photo.description}</div>
-            </div>
-        </div>
-    </li>
-    `).join('');
-    
-}
-
 (async () => { 
 
     const photoData = await fetchProfileData();
-    updatePhotos(photoData);
+    const photo = document.getElementById('photos');
+    const teste = photoData.content.map(photo => {
+        return `
+        <li class="item">
+            <div>
+                <div class="polaroid"><img src="${photo.image}">
+                    <div class="caption">${photo.description}</div>
+                </div>
+            </div>
+        </li>
+    
+        `}).join('');
+    photo.innerHTML = teste;
 })()
